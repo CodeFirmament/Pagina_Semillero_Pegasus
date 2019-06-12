dc9f49d8e5513dc814fba333107495434111128f
Jorge Gutierrez <workjorge@yahoo.com>
2019-06-12 09:44:15 -0500

    Primera version Qubit, basica

---
+++ script/PopUps.js
@@ -0,0 +1,11 @@
+function Acerca_Qubit(){
+	alert("Qubit es una empresa de venta de \n venta de elementos astronomicos");
+}
+
+function Pol_Cookies () {
+	alert("Podemos usar cookies segun el reglamento Europeo");
+}
+
+function TerminosCondiciones(){
+	alert("Al entrar aqui aceptas vendernos tu alma");
+}
