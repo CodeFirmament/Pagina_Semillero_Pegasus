cef6b798d6a5142ee32b6253de2dfa22bdac32e3
J Andres <workjorge@yahoo.com>
2019-06-14 23:04:17 -0500

    Version Compleja

--- script/PopUps.js
+++ script/PopUps.js
@@ -1,11 +1,36 @@
-function Acerca_Qubit(){
-	alert("Qubit es una empresa de venta de \n venta de elementos astronomicos");
+function Acerca_Qubit(Lenguaje){
+	if(Lenguaje=="es"){
+	 alert("Qubit es una empresa de venta de Articulos astronomicos");
+	}
+	if(Lenguaje=="en"){
+	 alert("Qubit has the objective of selling astronomic articles on the world");
+	}
+
 }
 
-function Pol_Cookies () {
-	alert("Podemos usar cookies segun el reglamento Europeo");
+function Pol_Cookies (Lenguaje) {
+   if(Lenguaje=="es"){
+	 alert("Podemos usar cookies segun el reglamento Europeo");
+	}
+   if(Lenguaje=="en"){
+   	 alert("We can use cookies as said in the European law");
+   	}
 }
 
-function TerminosCondiciones(){
-	alert("Al entrar aqui aceptas vendernos tu alma");
+function TerminosCondiciones(Lenguaje){
+    if(Lenguaje=="es"){
+	 alert("Al entrar aqui aceptas vendernos tu alma");
+    }
+	if(Lenguaje=="en"){
+	 alert("Your soul is ours now");
+	}
+}
+
+function Creditos(Lenguaje){
+	if (Lenguaje=="es") {
+	 alert("iconos: Egor Rumyantsev (CC) https://www.flaticon.com/authors/egor-rumyantsev, https://www.countryflags.com  fondo pantalla: https://wallhere.com/es/wallpaper/611568");
+	}
+	if(Lenguaje=="en"){
+     alert("icons: Egor Rumyantsev (CC) https://www.flaticon.com/authors/egor-rumyantsev, https://www.countryflags.com wallpaper: https://wallhere.com/es/wallpaper/611568");
+	}
 }
